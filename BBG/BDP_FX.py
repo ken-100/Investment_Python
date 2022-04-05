9c446c09707d043dbae2ba76a2ce45f85c5951ac
Ken.Y <69562939+ken-100@users.noreply.github.com>
2022-04-05 23:14:45 +0900

    Update BDP_FX.py

--- BBG/BDP_FX.py
+++ BBG/BDP_FX.py
@@ -3,11 +3,10 @@ from xbbg import blp
 con = pdblp.BCon(timeout=10000)
 con.start()
 
-LS = ["USD","EUR","GBP","CAD","AUD","NZD","CHF","NOK","SEK","HKD","CNY"]
+LS_EXJ = ["USD","EUR","GBP","CAD","AUD","NZD","CHF","NOK","SEK","HKD","CNH"]
 
 T = []
-T += [i + "JPY BGN Curncy" for i in LS]
-# T += ["CSBS" + i + " TMUQ Curncy" for i in LS]  #Mitsubishi TTM
+T += [i  + "JPY BGN Curncy" for i in LS_EXJ]
 
 tmp = ["px_last","volatility_90d","rsi_30d"]
 BDP = blp.bdp(tickers=T, flds=tmp)
@@ -15,8 +14,7 @@ BDP = BDP.loc[T,:]
 
 for i in tmp:
     BDP.loc[:,i.lower()] = round(BDP.loc[:,i.lower()].astype(float),2)
-
-BDP.insert(0, "Currency", LS)
-BDP.insert(0, "Ticker", T)
+BDP.insert(0, "Currency", LS_EXJ )
+BDP.insert(1, "Ticker", T)
 BDP.index = list(range(0, len(BDP)))
 BDP
