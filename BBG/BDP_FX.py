8bd48dac32571eb1d5356d169e00371f2de28483
Ken.Y <69562939+ken-100@users.noreply.github.com>
2022-04-13 22:59:57 +0900

    Update BDP_FX.py

--- BBG/BDP_FX.py
+++ BBG/BDP_FX.py
@@ -1,11 +1,12 @@
+from xbbg import blp
+
 con = pdblp.BCon(timeout=10000)
 con.start()
 
 LS_EXJ = ["USD","EUR","GBP","CAD","AUD","NZD","CHF","NOK","SEK","HKD","CNY"]
 
-T = []
-T += ["CSBS" + i + " TMUQ Curncy" for i in LS_EXJ]  #Mitsubishi TTM
-# T += [i  + "JPY CMPN Curncy" for i in LS_EXJ] #CMPN
+T = ["CSBS" + i + " TMUQ Curncy" for i in LS_EXJ]  #Mitsubishi TTM
+# T = [i  + "JPY CMPN Curncy" for i in LS_EXJ] #CMPN
 
 tmp = ["px_last","volatility_90d","rsi_30d"]
 BDP = blp.bdp(tickers=T, flds=tmp)
@@ -17,3 +18,6 @@ BDP.insert(0, "Currency", LS_EXJ )
 BDP.insert(1, "Ticker", T)
 BDP.index = list(range(0, len(BDP)))
 BDP
+
+# https://data.bloomberglp.com/labs/sites/2/2013/12/blpapi-developers-guide-1.38.pdf
+# https://data.bloomberglp.com/professional/sites/10/2017/03/BLPAPI-Core-Developer-Guide.pdf
