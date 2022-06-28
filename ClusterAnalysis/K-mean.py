01f6f82854c0fc441b0b04f300961971ec57da13
Ken.Y <69562939+ken-100@users.noreply.github.com>
2022-06-28 10:22:03 +0900

    Update K-mean.py

--- ClusterAnalysis/K-mean.py
+++ ClusterAnalysis/K-mean.py
@@ -12,7 +12,7 @@ c = KMeans(n_clusters=3,random_state=3).fit_predict(df) #pred_cluster
 # c = KMeans(n_clusters=3, init='k-means++', random_state=3).fit_predict(df) #pred_cluster # k-mean++
 
 M = pd.DataFrame(confusion_matrix(iris.target,c))
-M.index = iris_dataset.target_names
+M.index = iris.target_names
 S = accuracy_score(iris.target,c)
 print("accuracy_score=" + str(S))
 M
