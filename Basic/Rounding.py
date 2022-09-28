eef6d8ffcdc49f9d1caec701dc2d92d964b3d0d6
Ken.Y <69562939+ken-100@users.noreply.github.com>
2022-09-29 08:52:06 +0900

    Update Rounding.py

--- Basic/Rounding.py
+++ Basic/Rounding.py
@@ -1,15 +1,22 @@
 a = 7.123456789
-
 print(round(a,2))
 print(int(a))
 print(str(a))
-print(format(a, '.2f'))
-print(format(a, '.0%'))
+print(format(a, ".2f"))
+print(format(a, ".0%"))
 # 7.12
 # 7
 # 7.123456789
 # 7.12
 # 712%
+
+a = 1234.5678
+print("{:,}".format(a))
+print(format(a,","))
+print(format(a,",.2f"))
+# 1,234.5678
+# 1,234.5678
+# 1,234.57
 
 import math
 print(math.floor(a)) #Roundup
