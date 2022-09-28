99d07f38e762e7cd37696e4a1e158ea1f158103d
Ken.Y <69562939+ken-100@users.noreply.github.com>
2022-09-28 17:46:04 +0900

    Create for.py

---
+++ Basic/for.py
@@ -0,0 +1,30 @@
+for i in range(0,4):
+    print(i)
+# 0
+# 1
+# 2
+# 3
+
+
+for i in range(0,4,2):
+    print(i)
+# 0
+# 2
+    
+
+  for i in range(4,0,-2):
+    print(i)
+# 4
+# 2
+
+
+for i in [10,20]:
+    print(i)
+# 10
+# 20
+
+
+for i,k in enumerate([10,20]):
+    print(i,k)
+# 0 10
+# 1 20
