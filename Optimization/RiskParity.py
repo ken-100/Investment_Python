f1a19d3ef7f327b72309656b6db47a7d1d6e8cbe
Ken.Y <69562939+ken-100@users.noreply.github.com>
2022-10-20 16:37:37 +0900

    Update RiskParity.py

--- Optimization/RiskParity.py
+++ Optimization/RiskParity.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
 import scipy.optimize as sco
-# https://docs.scipy.org/doc/scipy-0.16.1/reference/generated/scipy.optimize.minimize.html
-
 from numpy import linalg
+# https://docs.scipy.org/doc/scipy-0.16.1/reference/generated/scipy.optimize.minimize.html
+# https://quantu.hateblo.jp/entry/2017/12/27/010526
 
 Cov = pd.DataFrame(np.zeros([4,4]))
 Cov.loc[0,:]=[0.0449016,0.0396086,0.0442209,0.0323200]
@@ -18,18 +18,30 @@ def f(w):
     return sum( (RC / SD - 1/len(w))**2 ) 
 
 B= [(0.001, 1)] * len(Cov)
-C = [{'type': 'eq', 'fun': lambda w: sum(w) - 1}]
-w0 =[1. / len(Cov)] * len(Cov)
+C = [{"type": "eq", "fun": lambda w: sum(w) - 1}]
 
-opts = sco.minimize(fun=f, x0=w0, method='SLSQP', bounds=B, constraints=C)
+
+SD0 = np.diag(Cov**0.5)
+tmp = 1/SD0   
+w0 = tmp/sum(tmp)
+
+opts = sco.minimize(fun=f, x0=w0, method="SLSQP", bounds=B, constraints=C)
 
 ww = opts["x"]
-print("Weight:",ww,"Net:",sum(ww))
 SD = np.dot(np.dot(np.array(ww),Cov),np.array(ww).T)**0.5
-RC = ww * (np.dot(np.array(ww),Cov)) / SD
-WA = sum(ww * np.diag(Cov**0.5))
-print("SD:",SD)
-print("RC:",RC,", Sum(RC):",sum(RC))
-print("RC/SD:",RC/SD)
-print("function:",opts["fun"])
-print("WA/SD:",WA/SD)
+
+tmp = ["Weight","SD","W*SD","RC","RC%"]
+Summary = pd.DataFrame(np.zeros([len(tmp),len(Cov)]),index=tmp)
+Summary.loc["Weight",:] = ww
+Summary.loc["SD",:] = SD0
+Summary.loc["W*SD",:] = ww * SD0
+Summary.loc["RC",:] = RC
+Summary.loc["RC%",:] = RC/SD
+
+for i in range(0,len(Summary)):
+    Summary.loc[Summary.index[i],"Net"] = sum( Summary.loc[Summary.index[i],list(range(0,len(Cov)))] )
+    Summary.iloc[i,:] = Summary.iloc[i,:].apply("{:.1%}".format)
+
+Summary.loc["SD","Net"] = "-"
+print("RiskParity")
+Summary
