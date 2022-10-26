0b0d32b3b874e519049d31438cb442d02d28e3a0
Ken.Y <69562939+ken-100@users.noreply.github.com>
2022-10-26 09:59:36 +0900

    Create Version.py

---
+++ etc/Version.py
@@ -0,0 +1,19 @@
+!python --version
+# Python 3.8.5
+
+
+import numpy as np
+print(np.__version__)
+# 1.23.4
+
+!pip show numpy
+# Name: numpy
+# Version: 1.23.4
+# Summary: NumPy is the fundamental package for array computing with Python.
+
+
+!pip list
+# Package                            Version
+# ---------------------------------- -------------------
+# absl-py                            1.3.0
+# alabaster                          0.7.12
