7644c144b99834d0db10a07ffe0de8583fb5afe1
Ken.Y <69562939+ken-100@users.noreply.github.com>
2022-10-28 11:46:05 +0900

    Update DataFrame.py

--- Basic/DataFrame.py
+++ Basic/DataFrame.py
@@ -5,8 +5,26 @@ import numpy as np
 # pd.set_option('display.max_rows', None)
 
 df = pd.DataFrame(columns = ["A","B"])
+
 df = pd.DataFrame(index = ["a","b"], columns = ["A","B"])
+#      A    B
+# a  NaN  NaN
+# b  NaN  NaN
+
 df = pd.DataFrame(np.zeros([2,2]), index = ["a","b"], columns = ["A","B"])
+#      A    B
+# a  0.0  0.0
+# b  0.0  0.0
+
+df = pd.DataFrame(np.zeros([2,2]))
+c = pd.MultiIndex.from_arrays([["A", ""], ["x","y"]])
+df.columns = c
+#      A     
+#      x    y
+# 0  0.0  0.0
+# 1  0.0  0.0
+
+
 for i in range(0,3):
     exec(f"df{i} = df")
 
@@ -25,4 +43,4 @@ print(a)
 # [[0 2]
 #  [4 6]]
 
-pd.DataFrame(a)
+df = pd.DataFrame(a)
