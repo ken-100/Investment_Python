1fa13eca0159e3ea4d22bdbabba559501d4df5ef
Ken.Y <69562939+ken-100@users.noreply.github.com>
2022-11-05 16:15:02 +0900

    Create Subplots.py

---
+++ Basic/Subplots.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#https://www.yutaka-note.com/entry/matplotlib_subplots
+
+x = np.linspace(-3,3)
+y1 = x**2
+y2 = x
+fig, ax = plt.subplots(1, 2, squeeze=False,figsize=(8,3),tight_layout=True)
+ax[0,0].plot(x, y1)
+ax[0,0].set_title("A")
+ax[0,1].plot(x, y2)
+ax[0,1].set_title("B")
+plt.show()
