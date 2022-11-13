d152dfd22f1a749b369ff0956b89490ca3318975
Ken.Y <69562939+ken-100@users.noreply.github.com>
2022-11-13 17:20:57 +0900

    Update and rename etcMatrix.py to etc/Matrix.py

---
+++ etc/Matrix.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+# Inverse Matrix
+arr = np.array([[2, 5], [1, 3]])
+arr_inv = np.linalg.inv(arr)
+print(arr_inv)
+# [[ 3. -5.]
+#  [-1.  2.]]
+
+
+
+# Transposed Matrix
+print(A.T)
+# [[2 1]
+#  [5 3]]
+
+
+
+# Matrix multiplication
+A = np.array([[1, 0, 0, 1], 
+              [0, 1,0, -1]])
+
+Sigma = np.array([[ 0.71, -0.743,  0.43,  0 ],
+                  [-0.43,  0.46,  -0.26,  0 ],
+                  [ 0.43 ,-0.26,  -0.46,  0 ],
+                  [    0,     0,      0, 0.2 ]
+                 ])
+
+np.dot( np.dot(A,Sigma), A.T)
+
+# array([[ 0.91 , -0.943],
+#        [-0.63 ,  0.66 ]])
+
+
+# Diagonal Matrix
+A = np.array([[3, 0],
+              [1, 2]])
+
+eig, P = np.linalg.eig(A)
+print("Eigenvalues\n",eig)
+print("\nEigenvectors\n",P)
+ 
+D = np.dot(np.dot(np.linalg.inv(P), A), P)
+print("\nDiagonal matrix\n",D)
