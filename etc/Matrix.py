a6bdbdd5a8faa6f20851f5e5a99f0f6643a4a7b7
Ken.Y <69562939+ken-100@users.noreply.github.com>
2022-11-16 21:36:06 +0900

    Update Matrix.py

--- etc/Matrix.py
+++ etc/Matrix.py
@@ -1,11 +1,12 @@
 import numpy as np
 
 # Inverse Matrix
-arr = np.array([[2, 5], [1, 3]])
-arr_inv = np.linalg.inv(arr)
-print(arr_inv)
+A = np.array([[2, 5], [1, 3]])
+A_inv = np.linalg.inv(A)
+print(A_inv)
 # [[ 3. -5.]
 #  [-1.  2.]]
+
 
 
 
