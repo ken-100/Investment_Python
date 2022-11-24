a8cdd9a79d6d382324b430ea56382f291d7b3183
Ken.Y <69562939+ken-100@users.noreply.github.com>
2022-11-24 10:22:35 +0900

    Create List.py

---
+++ Basic/List.py
@@ -0,0 +1,24 @@
+list(range(5))
+# [0, 1, 2, 3, 4]
+
+
+list(range(2,5))
+# [2, 3, 4]
+
+
+list(range(0,5,2))
+# [0, 2, 4]
+
+
+list(range(5,0,-1))
+# [5, 4, 3, 2, 1]
+
+
+[round(i*0.1,1) for i in range(5)]
+# [0.0, 0.1, 0.2, 0.3, 0.4]
+
+
+A = ["a","b"]
+B = ["c","d"]
+[x+y for x in A for y in B]
+# ['ac', 'ad', 'bc', 'bd']
