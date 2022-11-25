834fefb6e6846543310c38e2f2beedcd6f3b0ed2
Ken.Y <69562939+ken-100@users.noreply.github.com>
2022-11-25 15:59:30 +0900

    Update DataFrame.py

--- Basic/DataFrame.py
+++ Basic/DataFrame.py
@@ -89,6 +89,6 @@ print( df.loc[~(df[0].isin([0])),:] ) #not in
 #    0  1
 # 1  4  6
 
-print(df.loc[~(df[0].isin([0])),:].reset_index())
+print(df.loc[~(df[0].isin([0])),:].reset_index(drop=True))
 #    index  0  1
 # 0      1  4  6
