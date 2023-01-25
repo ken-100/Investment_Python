f12f042c1ce00ac57692179bd4043cf41aaee269
Ken.Y <69562939+ken-100@users.noreply.github.com>
2023-01-25 15:36:41 +0900

    Update DF_AddRows.py

--- Basic/DF_AddRows.py
+++ Basic/DF_AddRows.py
@@ -22,6 +22,9 @@ print(time.time() - start)
 # <ipython-input-167-7e0798bdf66b>:4: PerformanceWarning: DataFrame is highly fragmented.  This is usually the result of calling `frame.insert` many times, which has poor performance.  Consider joining all columns at once using pd.concat(axis=1) instead. To get a de-fragmented frame, use `newframe = frame.copy()`
 #   df.loc[:,j] = 0
 
+# C:\smtamsys\Anaconda3\lib\site-packages\pdblp\pdblp.py:278: FutureWarning: In a future version, `df.iloc[:, i] = newvals` will attempt to set the values inplace instead of always setting a new array. To retain the old behavior, use either `df[df.columns[i]] = newvals` or, if columns are non-unique, `df.isetitem(i, newvals)`
+#   df.loc[:, 'date'] = pd.to_datetime(df.loc[:, 'date'])
+
 
 #Poor example2
 start = time.time()
