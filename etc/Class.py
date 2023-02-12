fe309e8b0bd311bb8a7561687d5bb230864e949c
Ken.Y <69562939+ken-100@users.noreply.github.com>
2023-02-12 23:06:24 +0900

    Create Class.py

---
+++ etc/Class.py
@@ -0,0 +1,10 @@
+class Human:
+    def __init__(self, name, year):
+        self.name = name
+        self.year = year
+        
+human1 = Human("Kawada", 26)
+print(human1.name)
+print(human1.year)
+# Kawada
+# 26
