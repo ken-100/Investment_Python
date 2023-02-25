6451c4f89fd1496fa414562d33b9ae460b341ba3
Ken.Y <69562939+ken-100@users.noreply.github.com>
2023-02-25 15:39:15 +0900

    Create Memory.py

---
+++ etc/Memory.py
@@ -0,0 +1,9 @@
+import sys
+
+print("{}{: >25}{}{: >10}{}".format('|','Variable Name','|','Memory','|'))
+print(" ------------------------------------ ")
+for var_name in dir():
+    if not var_name.startswith("_") and sys.getsizeof(eval(var_name)) > 10000:
+        print("{}{: >25}{}{: >10}{}".format('|',var_name,'|',sys.getsizeof(eval(var_name)),'|'))
+        
+del A
