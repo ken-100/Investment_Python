30796e1271739694756db6b062f046b1db62bc69
Ken.Y <69562939+ken-100@users.noreply.github.com>
2023-03-13 09:54:14 +0900

    Update List.py

--- Basic/List.py
+++ Basic/List.py
@@ -42,3 +42,25 @@ np.zeros((3,4))
 # array([[0., 0., 0., 0.],
 #        [0., 0., 0., 0.],
 #        [0., 0., 0., 0.]])
+
+
+arr = [23, 6, 22, 3, 38, 15]
+
+# Number of matching elements
+n = sum(x>10 for x in arr)
+print(n)
+n = sum(1 for x in arr if x>10)
+print(n)
+n = sum([1 for x in arr if x>10])
+print(n)
+n = len([1 for x in arr if x>10])
+print(n)
+# 4
+# 4
+# 4
+# 4
+
+# Sum of matching elements
+n = sum(x for x in arr if x>10)
+print(n)
+# 98
