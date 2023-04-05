48ff45548dce75ffe606a8fe0746b1d3163b6801
Ken.Y <69562939+ken-100@users.noreply.github.com>
2023-04-05 23:08:41 +0900

    Update openpyx.py

--- Basic/IputOutput/openpyx.py
+++ Basic/IputOutput/openpyx.py
@@ -5,25 +5,23 @@ import numpy as np
 
 %cd C:\blp\data
 
-tmp =  ["Name","Asset","Ticker","Ticker2","HC","Alt","Fx","Fx_R","Flag"]
-T = pd.DataFrame(np.zeros([0,len(tmp)]),columns=tmp)
+
+    
+r = ws.max_row + 1   
+c = ws.max_column + 1
+
+#Read 
+pd.DataFrame(ws.values)
 
 
-wb = openpyxl.load_workbook("為替FH.xlsx",data_only=True)['forPython']
-for i in range(5,10):
-    
-    
-    
-    
-        if wb["B"+str(i)].value is None:
-            print(i)
-            break
-            
-for row in wb["A4:E8"]:
-    tmp = []
-    for col in row:
-        tmp.append(col.value)
-    print(tmp)
-    
-pd.DataFrame(tmp)
 
+for r in ws.rows:
+    print('----------------------------')
+    for cell in r:
+        print(cell.value)
+
+        
+for c in ws.columns:
+    print('----------------------------')
+    for cell in c:
+        print(cell.value)
