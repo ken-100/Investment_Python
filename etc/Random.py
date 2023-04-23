a93f7827eab235f26cb179a743c0e81055ab0885
Ken.Y <69562939+ken-100@users.noreply.github.com>
2023-04-24 00:29:56 +0900

    Create Random.py

---
+++ etc/Random.py
@@ -0,0 +1,20 @@
+import numpy as np
+i = 2
+np.random.seed(0)
+
+
+# Uniform distribution / More than 0.0, less than 1.0
+print(np.random.rand(i))
+print(np.random.rand(i,i))    
+print(np.random.random_sample(i))
+print(np.random.random(i))
+print("*2 × 2 is random.rand only\n")
+
+
+
+# Normal distribution / mean 0, variance 1 
+print(np.random.randn(i))
+print(np.random.randn(i,i))
+
+# https://note.nkmk.me/python-numpy-random/
+
