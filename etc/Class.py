6a6e1eae999dc71c9fac37a76521f973cf082b6c
Ken.Y <69562939+ken-100@users.noreply.github.com>
2023-04-23 23:41:08 +0900

    Update Class.py

--- etc/Class.py
+++ etc/Class.py
@@ -1,10 +1,38 @@
-class Human:
-    def __init__(self, name, year):
-        self.name = name
-        self.year = year
+# ex1
+class SimpleData:
+
+    a = 0
+    b = 0
+    
+    def set(self, a, b):
+        self.a = a
+        self.b = b
         
-human1 = Human("Kawada", 26)
-print(human1.name)
-print(human1.year)
-# Kawada
-# 26
+    def sum(self):
+        return self.a + self.b
+
+data1 = SimpleData()
+print(data1.sum())
+data1.set(1, 2)
+print(data1.sum())
+
+# 0
+# 3
+
+
+
+
+# ex2
+class SimpleData:
+    
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+        
+    def sum(self):
+        return self.a + self.b
+
+data1 = SimpleData(1,2)
+print(data1.sum())
+
+# 3
