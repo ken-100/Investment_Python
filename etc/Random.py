9105d8e2473e2adc013a7d5a0b1c81fb45900b88
Ken.Y <69562939+ken-100@users.noreply.github.com>
2023-04-29 16:41:58 +0900

    Update Random.py

--- etc/Random.py
+++ etc/Random.py
@@ -1,7 +1,9 @@
+# https://note.nkmk.me/python-numpy-random/
+
+
 import numpy as np
 i = 2
 np.random.seed(0)
-
 
 # Uniform distribution / More than 0.0, less than 1.0
 print(np.random.rand(i))
@@ -10,17 +12,9 @@ print(np.random.random_sample(i))
 print(np.random.random(i))
 print("*2 × 2 is random.rand only\n")
 
-
-
 # Normal distribution / mean 0, variance 1 
 print(np.random.randn(i))
 print(np.random.randn(i,i))
 
-
 # Multidimensional normal distribution
 np.random.multivariate_normal(np.zeros(i),np.identity(i)) #(mean, cov, size, check_valid, tol)
-
-
-
-# https://note.nkmk.me/python-numpy-random/
-
