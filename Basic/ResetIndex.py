8ff0c429596d75d59c4c516fa284cec7e427fdce
Ken.Y <69562939+ken-100@users.noreply.github.com>
2023-06-12 23:19:08 +0900

    Delete ResetIndex.py

--- Basic/ResetIndex.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-
-arr = np.arange(12).reshape((4, 3))
-df = pd.DataFrame(arr,columns=["x","y","z"])
-
-df = df.loc[df["x"] >= 4,:].reset_index(drop=True)
-df
