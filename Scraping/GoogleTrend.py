70a13fc61857f18f3e57f76a8c3df9a4e5db1d14
Ken.Y <69562939+ken-100@users.noreply.github.com>
2023-06-15 01:34:36 +0900

    Update GoogleTrend.py

--- Scraping/GoogleTrend.py
+++ Scraping/GoogleTrend.py
@@ -1,17 +1,16 @@
 from pytrends.request import TrendReq
 import pandas as pd
-
 # https://github.com/GeneralMills/pytrends
 # https://qiita.com/takubb/items/e5578a8143a4f6b0f7fc
 
 pytrends = TrendReq(hl='en-US', tz=360, timeout=(100,200) ) 
 KW = ["Bitcoin","Covid"]
-for i in range(0,len(KW)):
+for i in range(len(KW)):
 #     pytrends.build_payload([KW[i]], timeframe="today 1-m", geo='US')
     pytrends.build_payload([KW[i]], timeframe="now 7-d", geo='US')
     tmp = pytrends.interest_over_time()
     if i == 0:
-        df = tmp.iloc[:,0:1]
+        df = tmp.iloc[:,:1]
     else:
         df = pd.concat([df, tmp.iloc[:,0:1]], axis=1)
     
