f66b0101d6bd89d70e476dc89015366770543783
Ken.Y <69562939+ken-100@users.noreply.github.com>
2023-06-15 01:06:41 +0900

    Update Subplot.py

--- Basic/Chart/Subplot.py
+++ Basic/Chart/Subplot.py
@@ -7,18 +7,18 @@ con.start()
 T = ["USDJPY Curncy","EURJPY Curncy"]
 df = con.bdh(T, "px_open", "20200101", "20210101").reset_index()
 df.columns = ["data"] + T
-df.loc[:,"BM"] = 120
+df["BM"] = 120
 
-x = df.loc[:,"data"]
-fig = plt.figure()
+x = df["data"]
+fig = plt.figure(figsize=(10, 5))
 
 
 j = 1
 for i in range(0,2):
     exec(f"ax{i} = fig.add_subplot(1, 2, {j})")
-    exec(f"y = df.loc[:,T[i]]")
+    exec(f"y = df[T[i]]")
     exec(f"ax{i}.plot(x,y, color = 'Blue')")
-    exec(f"ax{i}.plot(x,df.loc[:,'BM'], color = 'Gray')")
+    exec(f"ax{i}.plot(x,df['BM'], color = 'Gray')")
     exec(f"ax{i}.set_title(i)")
     exec(f"ax{i}.set_ylim(100,140)")
     plt.xlabel("x-lable")
@@ -28,5 +28,4 @@ for i in range(0,2):
     
 plt.gcf().autofmt_xdate()
 fig.tight_layout()
-
 # https://www.yutaka-note.com/entry/matplotlib_axis
