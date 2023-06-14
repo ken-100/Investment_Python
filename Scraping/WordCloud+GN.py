10bf7c00ec6f6c5d0db5082e730b47943d702088
Ken.Y <69562939+ken-100@users.noreply.github.com>
2023-06-15 01:35:56 +0900

    Update WordCloud+GN.py

--- Scraping/WordCloud+GN.py
+++ Scraping/WordCloud+GN.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 url = []
 s = ["SP500+OR+(US+Stock)","Oil"]
 
-for i in range(0,len(s)):
+for i in range(len(s)):
     url += ["https://news.google.com/rss/search?q={"+s[i]+"}+when:3"]
 #     webbrowser.open("https://news.google.com/search?q="+s[i]+"%20when%3A3d&hl=en-US&gl=US&ceid=US%3Aen")
     
@@ -16,13 +16,13 @@ url += ["https://news.google.com/news/rss"]
 topic = ["WORLD","NATION","BUSINESS"]
 s = s + ["Headline"] + topic
 tmp = "https://news.google.com/news/rss/headlines/section/topic/"
-for i in range(0,len(topic)):
+for i in range(len(topic)):
     url += [tmp+topic[i]]
     
     
 rcParams['figure.figsize'] = 7,7
 
-for i in range(0,len(url)):
+for i in range(len(url)):
     d = feedparser.parse(url[i])
     news = list()
 
@@ -33,10 +33,10 @@ for i in range(0,len(url)):
         tmp = {"no": j, "title": entry.title, "summary": entry.summary, "link": entry.link,"published": entry.published, "link": entry.link,"sortkey": sortkey }
         news.append(tmp)
 
-    df = pd.DataFrame(news).loc[:,['title','link','sortkey']]
+    df = pd.DataFrame(news)[['title','link','sortkey']]
     
     WC = ""
-    for j in range(0,len(df)):
+    for j in range(len(df)):
         WC += df.loc[j,"title"]
 
     WC = WordCloud().generate(WC.lower())
@@ -45,4 +45,4 @@ for i in range(0,len(url)):
     plt.axis('off')
     print(str(i)+":"+s[i])
     plt.show()
-   
+  
