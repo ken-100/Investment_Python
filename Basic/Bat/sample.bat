d3461974e46342a960246ddf5c9180c7589581f3
Ken.Y <69562939+ken-100@users.noreply.github.com>
2023-07-17 15:13:57 +0900

    Update sample.bat

--- Basic/Bat/sample.bat
+++ Basic/Bat/sample.bat
@@ -4,10 +4,14 @@ rem Bloomberg
 start "" "C:\blp\Wintrv\wintrv.exe"
 
 rem Excel
-rem start "Excel" /B /max "C:\Program Files\Microsoft Office\Office15\EXCEL.EXE" /x "C:\Users\ky090\OneDrive\001_Market\US.xlsm"
-start "Excel" /B /max "EXCEL.EXE" /x "C:\Users\ky090\OneDrive - The University of Texas at Austin\041_ShortCut\010_Bat\test_forBat.xlsx"
+rem start "Excel" /B /max "C:\Program Files\Microsoft Office\Office15\EXCEL.EXE" /x "C:\Users\----\US.xlsm"
+start "Excel" /B /max "EXCEL.EXE" /x "C:\Users\----\010_Bat\test_forBat.xlsx"
 
 rem Chrome
 rem start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "https://www.nikkei.com/"
 start "" "chrome.exe" "https://www.nikkei.com/"
 
+rem Python
+call C:\Users\----\activate.bat
+cd C:\Users\----\020_Bat
+python NQ.py
