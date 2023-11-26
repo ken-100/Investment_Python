016835edd92325a6fdcba3862f790dacca27376d
Ken.Y <69562939+ken-100@users.noreply.github.com>
2023-11-26 17:00:18 +0900

    Update Version.py

--- Basic/Setting/Version.py
+++ Basic/Setting/Version.py
@@ -6,12 +6,21 @@ import numpy as np
 print(np.__version__)
 # 1.23.4
 
-
 !pip show numpy
 # Name: numpy
 # Version: 1.23.4
 # Summary: NumPy is the fundamental package for array computing with Python.
 
+
+!pip install gym==0.26.2
+import gym
+print(gym.__version__)
+# 0.26.2
+
+!pip install gym==0.12.1
+import gym
+print(gym.__version__)
+# 0.12.1
 
 !pip list
 # Package                            Version
