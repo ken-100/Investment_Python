390094375251487b7b10954650e66d46d93413f4
Ken.Y <69562939+ken-100@users.noreply.github.com>
2024-11-24 17:10:28 +0900

    Create Notification.py

---
+++ Sources_exBBG/Notification.py
@@ -0,0 +1,11 @@
+# First, install the plyer library
+# pip install plyer
+
+from plyer import notification
+
+# Display a notification
+notification.notify(
+    title='Hello',
+    message='Hello, Take a Break',
+    app_name='Python Coding'
+)
