2cded16823b5bce422aae8e1bf92a644eea39995
Ken.Y <69562939+ken-100@users.noreply.github.com>
2026-05-14 14:27:42 +0900

    Update Version.py

--- Basic/Setting/Version.py
+++ Basic/Setting/Version.py
@@ -27,3 +27,20 @@ print(gym.__version__)
 # ---------------------------------- -------------------
 # absl-py                            1.3.0
 # alabaster                          0.7.12
+
+
+!pip list | findstr xbbg
+# xbbg                      1.2.2
+
+
+!pip show xbbg
+# Name: xbbg
+# Version: 1.2.2
+# Summary: Independent client for Bloomberg-connected data workflows
+# Home-page: https://xbbg.org/
+# Author: 
+# Author-email: Alpha x1 <alpha.xone@outlook.com>
+# License-Expression: Apache-2.0
+# Location: C:\SMTAMSYS\Python313\Lib\site-packages
+# Requires: narwhals
+# Required-by: 
